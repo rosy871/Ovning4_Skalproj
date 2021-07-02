d4ed40ac7d1ae8e515969609a9716cc6c4e388be
rosy871 <yalameju@gmail.com>
2021-07-02 15:47:05 +0200

    added extension and made changes in original project

---
+++ SkalProj_Datastrukturer_Minne/Extensions.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SkalProj_Datastrukturer_Minne
+{
+    public static class Extensions
+    {
+        
+        public static bool CheckForParanthesis(this string str)
+        {
+            Dictionary<char, char> mapBrackets = new Dictionary<char, char>() { { '}', '{' },
+                                                                         { ']', '[' },
+                                                                         { ')', '(' } };
+
+            Stack<char> bracetStack = new Stack<char>();
+            foreach (char c in str)
+            {
+                if (c == '{' || c == '[' || c == '(')
+                    bracetStack.Push(c);
+                else if (c == '}' || c == ']' || c == ')')
+                {
+                    if (bracetStack.Count == 0)
+                        return false;
+                   
+                    char mappedBracketValue = mapBrackets[c];
+                   
+                    if (bracetStack.Peek() == mappedBracketValue)
+                    {
+                        bracetStack.Pop();
+                    }
+                    else
+                        return false;
+                }
+            }
+            if (bracetStack.Count > 0)
+                return false;
+
+            return true;
+        }
+
+
+        public static int RecursiveEven(this int num)
+        {
+            if (num == 0)
+            {
+                return 0;
+            }
+            return RecursiveEven(num - 1) + 2;
+        } 
+
+
+        public static int IterationEven(this int num)
+        {
+            if (num == 0)
+            {
+                return 0;
+            }
+            int result = 0;
+           for(int i = 0; i<num;i++)
+            {
+                result += 2;
+            }
+            return result;
+        }
+
+
+        public static int FibonacciSequence(this int num)
+        {
+            if (num == 0||num==1)
+            {
+                return 1;
+            }
+
+            return FibonacciSequence(num - 1)+FibonacciSequence(num-2);
+        }
+
+        public static int IterateFibonacciSequence(this int num)
+        {
+            int pnum = 0, ppnum= 0;
+            int res=1;
+           
+            for (int i = 0; i <  num; i++)
+            {
+                pnum = ppnum;
+                  
+                ppnum = res;
+
+                res = pnum+ppnum;
+            }
+            return res;
+        }
+    }
+}
+//Rekursion är slower än iteration på grund av overhead för att underhålla stacken. Rekursion använder mer minne än iteration men Rekursion gör koden mindre.
--- SkalProj_Datastrukturer_Minne/Program.cs
+++ SkalProj_Datastrukturer_Minne/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace SkalProj_Datastrukturer_Minne
 {
@@ -13,11 +15,15 @@ namespace SkalProj_Datastrukturer_Minne
 
             while (true)
             {
-                Console.WriteLine("Please navigate through the menu by inputting the number \n(1, 2, 3 ,4, 0) of your choice"
+                Console.Clear();
+
+                Console.WriteLine("Please navigate through the menu by inputting the number \n(1, 2, 3 ,4, 5, 6 0) of your choice"
                     + "\n1. Examine a List"
                     + "\n2. Examine a Queue"
                     + "\n3. Examine a Stack"
                     + "\n4. CheckParanthesis"
+                    + "\n5. CheckRekursive"
+                    + "\n6. CheckIterative"
                     + "\n0. Exit the application");
                 char input = ' '; //Creates the character input to be used with the switch-case below.
                 try
@@ -43,23 +49,40 @@ namespace SkalProj_Datastrukturer_Minne
                     case '4':
                         CheckParanthesis();
                         break;
-                    /*
-                     * Extend the menu to include the recursive 
-                     * and iterative exercises.
-                     */
+                    case '5':
+                        RekursionMethod();
+                        break;
+                    case '6':
+                        IterationMethod();
+                        break;
                     case '0':
                         Environment.Exit(0);
                         break;
                     default:
-                        Console.WriteLine("Please enter some valid input (0, 1, 2, 3, 4)");
+                        Console.WriteLine("Please enter some valid input (0, 1, 2, 3, 4), 5, 6");
                         break;
                 }
             }
         }
 
+      
+
         /// <summary>
         /// Examines the datastructure List
         /// </summary>
+
+        // 2.   först är listans kapacitet 4. det betyder att när listan skapas skapar den faktiskt en underliggande array med storlek 4
+        //      och när liststorleken blir 4 kommer den att öka sin kapacitet med dubbelt så stor som den tidigare arrayen.
+        //      det betyder att det först skapar 4 storleks array och när det behövs för att lägga till mer data  skapar den sedan 8 storleks arrat
+        //      och därefter skapas 16 och så vidare.
+
+        //3.    kapaciteten okar med dubbelt så stor som tidigare kapaciteten.
+
+        //4.
+        //
+        //5.    nej, kapaciteten inte minskar när element tas bort ur listan.
+        //
+        //6.    om storlek är fixed sen det är bättre att använda array
         static void ExamineList()
         {
             /*
@@ -72,29 +95,151 @@ namespace SkalProj_Datastrukturer_Minne
              * Below you can see some inspirational code to begin working.
             */
 
-            //List<string> theList = new List<string>();
-            //string input = Console.ReadLine();
+            //list<string> thelist = new list<string>();
+            //string input = console.readline();
             //char nav = input[0];
             //string value = input.substring(1);
 
-            //switch(nav){...}
+            var userinputlist = new List<string>();
+
+            Console.WriteLine($"list capacity: {userinputlist.Capacity}");
+            Console.WriteLine("\nEnter the +inputstring to add in the list ,\n-inputstring to delete from the list and \npress e to Exit");
+
+            do
+            {
+                string userinput = Console.ReadLine();
+                char nav = userinput[0];
+                string value = userinput.Substring(1);
+               
+                if (value.Length <= 0 && nav!='e')
+                {
+                    Console.WriteLine("Fel input type ...");
+                }
+                else
+                {
+                    switch (nav)
+                    {
+                        case '+':
+                            userinputlist.Add(value);
+                            Console.WriteLine($"Added {value} to list. \tList Count: {userinputlist.Count}. \tLlist Capacity: {userinputlist.Capacity}. ");
+                            break;
+
+                        case '-':
+                            if (userinputlist.Contains(value))
+                            {
+                                userinputlist.Remove(value);
+                                Console.WriteLine($"Removed {value} from list .\t List Count: {userinputlist.Count}. \tList Capacity: {userinputlist.Capacity}. ");
+                            }
+                            else
+                                Console.WriteLine($" List does not contain in the {value}");
+
+                            break;
+
+                        case 'e':
+                            return;
+
+                        default:
+                            Console.WriteLine("Fel input.. type string that begins with + and - sign to add and remove strings in the list");
+                            break;
+                    }
+
+                }
+            } while (true);
         }
 
         /// <summary>
         /// Examines the datastructure Queue
         /// </summary>
+        /// 
+       
         static void ExamineQueue()
         {
             /*
              * Loop this method untill the user inputs something to exit to main menue.
              * Create a switch with cases to enqueue items or dequeue items
              * Make sure to look at the queue after Enqueueing and Dequeueing to see how it behaves
-            */
+            
+             */
+            SimulateICAQueue();
+            TestQueue();
         }
+
+        private static void TestQueue()
+        {
+            Console.WriteLine("\nEnter the +name to enter the queue ,\n- to exit from the queue and \nPress e to Exit");
+
+            var que = new Queue<string>();
+            do
+            {
+                string userinput = Console.ReadLine();
+
+                char nav = userinput[0];
+
+                string value = userinput.Substring(1);
+
+                if (value.Length <= 0 && nav != 'e'&& nav != '-')
+                {
+                    Console.WriteLine("Fel input type ...");
+                }
+                else
+                {
+                    switch (nav)
+                    {
+                        case '+':
+                            que.Enqueue(value);
+                            Console.WriteLine($"{value} ställer sig i kö and Queue count: {que.Count}");
+                            break;
+
+                        case '-':
+
+                            if (que.Count <= 0)
+                                Console.WriteLine("Queue is empty");
+                            else
+                            {
+                                var name = que.Dequeue();
+                                Console.WriteLine($"{name} blir expedieriad och lämnar kön");
+                            }
+                            break;
+
+                        case 'e':
+                            return;
+
+                        default:
+                            Console.WriteLine("Fel input.. type a name that begins with + to Add in queue and press - sign to remove from the queue");
+                            break;
+                    }
+
+                }
+            } while (true);
+        }
+
+        private static void SimulateICAQueue()
+        {
+            string name;
+            Console.WriteLine("ICA oppnar och kassan till kön är tom.");
+            var IcaQue = new Queue<string>();
+            IcaQue.Enqueue("Kalle");
+            Console.WriteLine("Kalle ställer sig i kön");
+            IcaQue.Enqueue("Greta");
+            Console.WriteLine("Greta ställer sig i kön");
+            name = IcaQue.Dequeue();
+            Console.WriteLine($"{name} blir expedierad och lämnar kön");
+            IcaQue.Enqueue("Olle");
+            Console.WriteLine("Olle ställer sig i kön");
+            while (IcaQue.Count > 0)
+            {
+                name = IcaQue.Dequeue();
+                Console.WriteLine($"{name} blir expedierad och lämnar kön");
+            }
+
+        }
+
 
         /// <summary>
         /// Examines the datastructure Stack
         /// </summary>
+        /// 
+        //det är fel  om sista peroson som stå att qö blir expedieriad first så att använda en stack i den här fället(for ICA kön) är inte smart.
         static void ExamineStack()
         {
             /*
@@ -102,6 +247,29 @@ namespace SkalProj_Datastrukturer_Minne
              * Create a switch with cases to push or pop items
              * Make sure to look at the stack after pushing and and poping to see how it behaves
             */
+
+            Stack<char> revstring = new Stack<char>();
+            var que = new Queue<string>();
+            do
+            {
+                Console.WriteLine("\nEnter the word that you want to reverse . Press e to Exit");
+                string userinput = Console.ReadLine();
+                if (userinput == "e")
+                    return;
+                foreach (char c in userinput)
+                {
+                    revstring.Push(c);
+                }
+
+                while (revstring.Count > 0)
+                {
+                    char rc = revstring.Pop();
+                    Console.Write(rc);
+                }
+                Console.WriteLine("");
+
+            } while (true);
+
         }
 
         static void CheckParanthesis()
@@ -111,9 +279,50 @@ namespace SkalProj_Datastrukturer_Minne
              * Example of correct: (()), {}, [({})],  List<int> list = new List<int>() { 1, 2, 3, 4 };
              * Example of incorrect: (()]), [), {[()}],  List<int> list = new List<int>() { 1, 2, 3, 4 );
              */
+            do
+            {
+                Console.WriteLine("\nEnther sentence to check the parantheses. \n Press e to Exit");
+                string str = Console.ReadLine();
+                if (str == "e")
+                    return;
+                bool chk = str.CheckForParanthesis();
+
+                if (chk)
+                    Console.WriteLine($"{str} är Välformed sträng");
+                else
+                    Console.WriteLine($"{str} är Ej välformed sträng");
+            } while (true);
 
         }
 
+        private static void IterationMethod()
+        {
+            Console.WriteLine("Enter en num");
+            int num = int.Parse(Console.ReadLine());
+
+            int res1 = num.IterationEven();
+            Console.WriteLine($"Result {num}th Even nummer: {res1} ");
+
+            int res2 = num.IterateFibonacciSequence();
+            Console.WriteLine($"Result {num}th fibonnacisekvensen: {res2}");
+
+            Console.ReadKey();
+        }
+
+        private static void RekursionMethod()
+        {
+            Console.WriteLine("Enter en num");
+            int num = int.Parse(Console.ReadLine());
+
+            int res1 = num.RecursiveEven();
+            Console.WriteLine($"Result {num}th Even nummer: {res1} ");
+
+            int res2 = num.FibonacciSequence();
+            Console.WriteLine($"Result {num}th fibonnacisekvensen: {res2}");
+            Console.ReadKey();
+
+        }
     }
 }
+
 
